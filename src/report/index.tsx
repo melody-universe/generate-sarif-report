16e3be61ed7e65fc73275e825f76f75a9e45db55
melody-universe <tyler.hogsett@microsoft.com>
2021-09-10 22:46:37 -0700

    logs might not be an array

--- src/report/index.tsx
+++ src/report/index.tsx
@@ -4,6 +4,6 @@ import { Viewer as SarifViewer } from "@microsoft/sarif-web-component";
 import "./index.scss";
 
 ReactDom.render(
-  <SarifViewer logs={sarifLogs} />,
+  <SarifViewer logs={[sarifLogs].flat()} />,
   document.getElementById("app")
 );
